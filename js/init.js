c96f1294c14f20115bd19db857b514b7e9dffd2e
Tenzin Ukyab <tsu@mit.edu>
2017-10-29 15:48:07 -0400

    actual initial commit

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.scrollspy').scrollSpy();
+    $('.collapsible').collapsible();
+    $('.materialboxed').materialbox();
+    $('.slider').slider();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
